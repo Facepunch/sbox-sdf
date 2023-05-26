3581d65e5ca4a5075ecb5094114c6a9502370981
James King <james.king@facepunchstudios.com>
2023-05-26 10:54:50 +0100

    Abstracted out SdfArray from Sdf2DArray

--- libsdf/code/2D/Sdf2DArray.cs
+++ libsdf/code/2D/Sdf2DArray.cs
@@ -7,101 +7,43 @@ internal record struct SdfArray2DData( byte[] Samples, int BaseIndex, int RowStr
 	public byte this[int x, int y] => Samples[BaseIndex + x + y * RowStride];
 }
 
-internal partial class Sdf2DArray : BaseNetworkable, INetworkSerializer
+internal partial class Sdf2DArray : SdfArray
 {
-	private const byte MaxEncoded = 255;
-	public const int Margin = 1;
-
-	public WorldQuality Quality { get; private set; }
-
-	private byte[] _samples;
-
-	private int _arraySize;
-	private float _unitSize;
-	private float _invUnitSize;
-	private float _invMaxDistance;
-
-	private bool _textureInvalid = true;
-	private Texture _texture;
-
-	public int ModificationCount { get; set; }
-
 	public Sdf2DArray()
 	{
 	}
 
 	public Sdf2DArray( WorldQuality quality )
+		: base( quality )
 	{
-		Init( quality );
+
 	}
 
-	public Texture Texture
+	protected override Texture CreateTexture()
 	{
-		get
-		{
-			if ( !_textureInvalid && _texture != null ) return _texture;
-
-			_textureInvalid = false;
-
-			if ( _texture == null )
-				_texture = new Texture2DBuilder()
-					.WithFormat( ImageFormat.I8 )
-					.WithSize( _arraySize, _arraySize )
-					.WithData( _samples )
-					.WithAnonymous( true )
-					.Finish();
-			else
-				_texture.Update( _samples );
-
-			return _texture;
-		}
+		return new Texture2DBuilder()
+			.WithFormat( ImageFormat.I8 )
+			.WithSize( ArraySize, ArraySize )
+			.WithData( Samples )
+			.WithAnonymous( true )
+			.Finish();
 	}
 
-	private void Init( WorldQuality quality )
+	protected override void UpdateTexture( Texture texture )
 	{
-		if ( Quality.Equals( quality ) )
-		{
-			return;
-		}
-
-		Quality = quality;
-
-		_arraySize = Quality.ChunkResolution + Margin * 2 + 1;
-		_unitSize = Quality.ChunkSize / Quality.ChunkResolution;
-		_invUnitSize = Quality.ChunkResolution / Quality.ChunkSize;
-		_invMaxDistance = 1f / Quality.MaxDistance;
-
-		_samples = new byte[_arraySize * _arraySize];
-
-		Clear( false );
-	}
-
-	private byte Encode( float distance )
-	{
-		return (byte)((int)((distance * _invMaxDistance * 0.5f + 0.5f) * MaxEncoded)).Clamp( 0, 255 );
-	}
-
-	private float Decode( byte encoded )
-	{
-		return (encoded * (1f / MaxEncoded) - 0.5f) * Quality.MaxDistance * 2f;
-	}
-
-	public void Clear( bool solid )
-	{
-		Array.Fill( _samples, solid ? (byte)0 : (byte)255 );
-		++ModificationCount;
+		texture.Update( Samples );
 	}
 
 	private (int MinX, int MinY, int MaxX, int MaxY) GetSampleRange( Rect bounds )
 	{
-		var min = (bounds.TopLeft - Quality.MaxDistance) * _invUnitSize;
-		var max = (bounds.BottomRight + Quality.MaxDistance) * _invUnitSize;
+		var min = (bounds.TopLeft - Quality.MaxDistance) * InvUnitSize;
+		var max = (bounds.BottomRight + Quality.MaxDistance) * InvUnitSize;
 
 		var minX = Math.Max( 0, (int)MathF.Ceiling( min.x ) + Margin );
 		var minY = Math.Max( 0, (int)MathF.Ceiling( min.y ) + Margin );
 
-		var maxX = Math.Min( _arraySize, (int)MathF.Ceiling( max.x ) + Margin );
-		var maxY = Math.Min( _arraySize, (int)MathF.Ceiling( max.y ) + Margin );
+		var maxX = Math.Min( ArraySize, (int)MathF.Ceiling( max.x ) + Margin );
+		var maxY = Math.Min( ArraySize, (int)MathF.Ceiling( max.y ) + Margin );
 
 		return (minX, minY, maxX, maxY);
 	}
@@ -116,21 +58,21 @@ internal partial class Sdf2DArray : BaseNetworkable, INetworkSerializer
 
 		for ( var y = minY; y < maxY; ++y )
 		{
-			var worldY = (y - Margin) * _unitSize;
+			var worldY = (y - Margin) * UnitSize;
 
-			for ( int x = minX, index = minX + y * _arraySize; x < maxX; ++x, ++index )
+			for ( int x = minX, index = minX + y * ArraySize; x < maxX; ++x, ++index )
 			{
-				var worldX = (x - Margin) * _unitSize;
+				var worldX = (x - Margin) * UnitSize;
 				var sampled = sdf[new Vector2( worldX, worldY )];
 
 				if ( sampled >= maxDist ) continue;
 
 				var encoded = Encode( sampled );
 
-				var oldValue = _samples[index];
+				var oldValue = Samples[index];
 				var newValue = Math.Min( encoded, oldValue );
 
-				_samples[index] = newValue;
+				Samples[index] = newValue;
 
 				changed |= oldValue != newValue;
 			}
@@ -138,8 +80,7 @@ internal partial class Sdf2DArray : BaseNetworkable, INetworkSerializer
 
 		if ( changed )
 		{
-			++ModificationCount;
-			_textureInvalid = true;
+			MarkChanged();
 		}
 
 		return changed;
@@ -155,21 +96,21 @@ internal partial class Sdf2DArray : BaseNetworkable, INetworkSerializer
 
 		for ( var y = minY; y < maxY; ++y )
 		{
-			var worldY = (y - Margin) * _unitSize;
+			var worldY = (y - Margin) * UnitSize;
 
-			for ( int x = minX, index = minX + y * _arraySize; x < maxX; ++x, ++index )
+			for ( int x = minX, index = minX + y * ArraySize; x < maxX; ++x, ++index )
 			{
-				var worldX = (x - Margin) * _unitSize;
+				var worldX = (x - Margin) * UnitSize;
 				var sampled = sdf[new Vector2( worldX, worldY )];
 
 				if ( sampled >= maxDist ) continue;
 
 				var encoded = Encode( sampled );
 
-				var oldValue = _samples[index];
+				var oldValue = Samples[index];
 				var newValue = Math.Max( (byte)(MaxEncoded - encoded), oldValue );
 
-				_samples[index] = newValue;
+				Samples[index] = newValue;
 
 				changed |= oldValue != newValue;
 			}
@@ -177,8 +118,7 @@ internal partial class Sdf2DArray : BaseNetworkable, INetworkSerializer
 
 		if ( changed )
 		{
-			++ModificationCount;
-			_textureInvalid = true;
+			MarkChanged();
 		}
 
 		return changed;
@@ -186,24 +126,7 @@ internal partial class Sdf2DArray : BaseNetworkable, INetworkSerializer
 
 	public void WriteTo( Sdf2DMeshWriter writer, Sdf2DLayer layer, bool renderMesh, bool collisionMesh )
 	{
-		writer.Write( new SdfArray2DData( _samples, Margin * _arraySize + Margin, _arraySize ),
+		writer.Write( new SdfArray2DData( Samples, Margin * ArraySize + Margin, ArraySize ),
 			layer, renderMesh, collisionMesh );
-	}
-
-	public void Read( ref NetRead net )
-	{
-		Init( WorldQuality.Read( ref net ) );
-
-		_samples = net.ReadUnmanagedArray( _samples );
-
-		++ModificationCount;
-		_textureInvalid = true;
-	}
-
-	public void Write( NetWrite net )
-	{
-		Quality.Write( net );
-
-		net.WriteUnmanagedArray( _samples );
 	}
 }
---
+++ libsdf/code/SdfArray.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Sdf;
+
+internal abstract partial class SdfArray : BaseNetworkable, INetworkSerializer
+{
+	protected const byte MaxEncoded = 255;
+	public const int Margin = 1;
+
+	public WorldQuality Quality { get; private set; }
+	public byte[] Samples { get; private set; }
+	public int ModificationCount { get; set; }
+	public int ArraySize { get; private set; }
+
+	protected float UnitSize { get; private set; }
+	protected float InvUnitSize { get; private set; }
+	protected float InvMaxDistance { get; private set; }
+
+	private bool _textureInvalid = true;
+	private Texture _texture;
+
+	protected SdfArray()
+	{
+
+	}
+
+	protected SdfArray( WorldQuality quality )
+	{
+		Init( quality );
+	}
+
+	protected byte Encode( float distance )
+	{
+		return (byte) ((int) ((distance * InvMaxDistance * 0.5f + 0.5f) * MaxEncoded)).Clamp( 0, 255 );
+	}
+
+	protected float Decode( byte encoded )
+	{
+		return (encoded * (1f / MaxEncoded) - 0.5f) * Quality.MaxDistance * 2f;
+	}
+
+	public Texture Texture
+	{
+		get
+		{
+			if ( !_textureInvalid && _texture != null ) return _texture;
+
+			_textureInvalid = false;
+
+			if ( _texture == null )
+				_texture = CreateTexture();
+			else
+				UpdateTexture( _texture );
+
+			return _texture;
+		}
+	}
+
+	protected abstract Texture CreateTexture();
+
+	protected abstract void UpdateTexture( Texture texture );
+
+	private void Init( WorldQuality quality )
+	{
+		if ( Quality.Equals( quality ) )
+		{
+			return;
+		}
+
+		Quality = quality;
+
+		ArraySize = Quality.ChunkResolution + Margin * 2 + 1;
+		UnitSize = Quality.ChunkSize / Quality.ChunkResolution;
+		InvUnitSize = Quality.ChunkResolution / Quality.ChunkSize;
+		InvMaxDistance = 1f / Quality.MaxDistance;
+
+		Samples = new byte[ArraySize * ArraySize];
+
+		Clear( false );
+	}
+
+	public void Clear( bool solid )
+	{
+		Array.Fill( Samples, solid ? (byte) 0 : (byte) 255 );
+		++ModificationCount;
+	}
+
+	protected void MarkChanged()
+	{
+		++ModificationCount;
+		_textureInvalid = true;
+	}
+
+	public void Read( ref NetRead net )
+	{
+		Init( WorldQuality.Read( ref net ) );
+
+		Samples = net.ReadUnmanagedArray( Samples );
+
+		MarkChanged();
+	}
+
+	public void Write( NetWrite net )
+	{
+		Quality.Write( net );
+
+		net.WriteUnmanagedArray( Samples );
+	}
+}
